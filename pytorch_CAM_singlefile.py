1415ce737971d2372a2abc9b2324a133b85d390c
Anudeep Das <anudeepdas2000@gmail.com>
2022-04-03 12:29:32 -0400

    Incorporating the successful ResNet model for CAM. Produced a CAM program that works for still images as well

--- pytorch_CAM_singlefile.py
+++ pytorch_CAM_singlefile.py
@@ -1,21 +1,16 @@
-# simple implementation of CAM in PyTorch for the networks such as ResNet, DenseNet, SqueezeNet, Inception
-# last update by BZ, June 30, 2021
-
 import io
 from PIL import Image
-from torchvision import models, transforms, datasets
+from torchvision import models, transforms
 from torch.autograd import Variable
 from torch.nn import functional as F
 import numpy as np
 import cv2
-import os
-import torch
 import json
+import torch
 
 # input image
 LABELS_file = 'imagenet-simple-labels.json'
-image_file = 'test.jpg'
-
+image_file = 'testBiden.jpg'
 # networks such as googlenet, resnet, densenet already use global average pooling at the end, so CAM could be used directly.
 model_id = 2
 if model_id == 1:
@@ -28,30 +23,24 @@ elif model_id == 2:
 elif model_id == 3:
     net = models.densenet161(pretrained=True)
     finalconv_name = 'features'
-
 net.eval()
-
 # hook the feature extractor
 features_blobs = []
 def hook_feature(module, input, output):
     features_blobs.append(output.data.cpu().numpy())
 
-# This allows us to get the output blobs?
 net._modules.get(finalconv_name).register_forward_hook(hook_feature)
 
-# get the softmax weight- the second last param
+# get the softmax weight
 params = list(net.parameters())
 weight_softmax = np.squeeze(params[-2].data.numpy())
 
 def returnCAM(feature_conv, weight_softmax, class_idx):
     # generate the class activation maps upsample to 256x256
     size_upsample = (256, 256)
-    # batch size? num_channels, height, width
     bz, nc, h, w = feature_conv.shape
     output_cam = []
     for idx in class_idx:
-        # weight_softmax[idx] holds the probability that the ground truth classification
-        #   if the image is idx
         cam = weight_softmax[idx].dot(feature_conv.reshape((nc, h*w)))
         cam = cam.reshape(h, w)
         cam = cam - np.min(cam)
@@ -59,8 +48,6 @@ def returnCAM(feature_conv, weight_softmax, class_idx):
         cam_img = np.uint8(255 * cam_img)
         output_cam.append(cv2.resize(cam_img, size_upsample))
     return output_cam
-
-
 normalize = transforms.Normalize(
    mean=[0.485, 0.456, 0.406],
    std=[0.229, 0.224, 0.225]
@@ -71,59 +58,34 @@ preprocess = transforms.Compose([
    normalize
 ])
 
+# load test image
+img_pil = Image.open(image_file)
+img_tensor = preprocess(img_pil)
+img_variable = Variable(img_tensor.unsqueeze(0))
+logit = net(img_variable)
 
 # load the imagenet category list
 with open(LABELS_file) as f:
     classes = json.load(f)
 
-classes = ['Female', 'Male']
+classes = ['female','male']
 
-# Attempting to access webcam
-cap = cv2.VideoCapture(0)
-
-while True:
-    #reads the code frame-by-frame
-    ret, frame = cap.read()
-    
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-    # gray_pil = Image.fromarray(gray)
-
-    pil = Image.fromarray(frame)
-    img_tensor = preprocess(pil)
-    img_variable = Variable(img_tensor.unsqueeze(0))
-    logit = net(img_variable)
-    print(classes[torch.argmax(logit).item()])
-
-    h_x = F.softmax(logit, dim=1).data.squeeze()
-    probs, idx = h_x.sort(0, True)
-    probs = probs.numpy()
-    idx = idx.numpy()
-
-    # generate class activation mapping for the top1 prediction
-    CAMs = returnCAM(features_blobs[0], weight_softmax, [idx[0]])
-
-    # render the CAM and output
-    # print('output CAM.jpg for the top1 prediction: %s'%classes[idx[0]])
-    # img = cv2.imread('test.jpg')
-    height, width, _ = frame.shape
-    resized = cv2.resize(CAMs[0],(width, height))
-    heatmap = cv2.applyColorMap(resized, cv2.COLORMAP_JET)
-
-    camresult = cv2.addWeighted(heatmap, 0.3, frame, 0.5,0)
-
-    # Originally, the CAM.jpg worked with "result". Webcam does not look additions and multiplications
-    # result = heatmap * 0.3 + frame * 0.5
-    cv2.imwrite('CAM2.jpg', camresult)
-    cv2.imshow('CAM', camresult)
-
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+h_x = F.softmax(logit, dim=1).data.squeeze()
+probs, idx = h_x.sort(0, True)
+probs = probs.numpy()
+idx = idx.numpy()
 
 # output the prediction
 # for i in range(0, 5):
 #     print('{:.3f} -> {}'.format(probs[i], classes[idx[i]]))
 
+# generate class activation mapping for the top1 prediction
+CAMs = returnCAM(features_blobs[0], weight_softmax, [idx[0]])
 
+# render the CAM and output
+print('output CAM.jpg for the top1 prediction: %s'%classes[idx[0]])
+img = cv2.imread(image_file)
+height, width, _ = img.shape
+heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
+result = heatmap * 0.3 + img * 0.5
+cv2.imwrite('CAM_biden.jpg', result)
